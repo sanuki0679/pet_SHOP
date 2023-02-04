600f6cf486c9fa17f2c3c845a36a424777eebe26
sanuki0679 <sanuki0679@gmail.com>
2023-02-04 15:32:53 +0900

    ペットショップ2課題完了

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     <h2>本日のご紹介ペット</h2>
 
     <form action="" method="get">
-
+        <label for="">キーワード</label>
         <input type="search" name="keyword">
         <input type="submit" name="submit" value="検索">
     </form>
@@ -32,22 +32,22 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         // データベースに接続
         $dbh = connect_db();
         $keyword_param = '%' . $keyword . '%';
-        
-        //$birthplace = $keyword;
 
+
+        if (($keyword) != '')  {
         // SQL文の組み立て
-        //$sql = 'SELECT * FROM animals';
-        //$sql = 'SELECT * FROM animals WHERE description LIKE:keyword_param';
-        $sql = 'SELECT * FROM animals WHERE description LIKE keyword_param = :keyword_param';
-        //$sql = 'SELECT * FROM animals WHERE birthplace = :birthplace';
-        
+        $sql = 'SELECT * FROM animals WHERE description LIKE :keyword_param';
+        $stmt = $dbh->prepare($sql);
+
+        $stmt->bindParam("keyword_param", $keyword_param, PDO::PARAM_STR);
+
+        }else {
+        $sql = 'SELECT * FROM animals';
+
         // プリペアドステートメントの準備
         // $dbh->query($sql) でも良い
         $stmt = $dbh->prepare($sql);
-        
-        //$stmt->bindParam("keyword_param", $keyword_param, PDO::PARAM_STR);
-        $stmt->bindParam("keyword_param", $keyword_param);
-        //$stmt->bindParam("birthplace", $birthplace,PDO::PARAM_STR);
+        }
 
         // プリペアドステートメントの実行
         $stmt->execute();
@@ -55,16 +55,14 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
         // 結果の受け取り
         $animals = $stmt->fetchAll(PDO::FETCH_ASSOC);
         ?>
-        <?php foreach ($animals as $animals) : ?>
+        <?php foreach ($animals as $animals): ?>
             <?= h($animals['type']) . 'の' . h($animals['classification']) . 'ちゃん' ?><br>
             <?= h($animals['description'])  ?><br>
             <?= h($animals['birthday']) . ' 生まれ'  ?><br>
             <?= '出身地 ' . h($animals['birthplace'])  ?><br>
             <hr width="100%">
         <?php endforeach; ?>
-
+        
     </div>
-
-
 
 </body>
